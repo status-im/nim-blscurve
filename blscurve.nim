e2ddcc468f0cbf86b912364eaf46b74b0bd1ff27
cheatfate <ka@hardcore.kiev.ua>
2019-01-30 14:15:10 +0200

    Add Milagro submodule. Add main export blscurve.nim.

---
+++ blscurve.nim
@@ -0,0 +1,10 @@
+# Nim-BLSCurve
+# Copyright (c) 2018 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+import blscurve/[common, bls]
+export common, bls
