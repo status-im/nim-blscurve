c0dd2c5339abf63781142e4aeb514bf82b86b470
mratsim <mamy_github@numforge.co>
2019-02-05 18:14:15 +0100

    Delete old milagro files

--- milagro_crypto.nim
+++
@@ -1,7 +0,0 @@
-# milagro_crypto
-# Copyright (c) 2018 Status Research & Development GmbH
-# Licensed and distributed under the Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
-# This file may not be copied, modified, or distributed except according to those terms.
-
-import milagro_crypto/[common, scheme3]
-export common, scheme3
