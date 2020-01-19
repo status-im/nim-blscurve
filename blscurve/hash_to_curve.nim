365ecebe9669ac577194a9e3d28c180474591abf
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-01-20 00:35:28 +0100

    Add the 3-isogeny map G2 constants

--- blscurve/hash_to_curve.nim
+++ blscurve/hash_to_curve.nim
@@ -118,6 +118,16 @@ proc toFP2(x, y: uint64): FP2_BLS381 =
 
   result.fromBigs(xBig, yBig)
 
+proc hexToFP2(x, y: string): FP2_BLS381 =
+  ## Convert a complex tuple x + iy to FP2
+  # TODO: the result does not seem to need zero-initialization
+  var xBig, yBig: BIG_384
+
+  discard xBig.fromHex(x)
+  discard yBig.fromHex(y)
+
+  result.fromBigs(xBig, yBig)
+
 proc isSquare(a: FP2_BLS381): bool =
   ## Returns true if ``a`` is a square in the FP2 field
   ## This is NOT a constant-time operation (Milagro has branches)
@@ -144,9 +154,10 @@ proc isNeg(a: FP2_BLS381): bool =
   let neg = neg(a)
   result = cmp(a, neg) < 0
 
-func mapToCurveSimpleSWU_G2(u: FP2_BLS381): ECP2_BLS381 =
+func mapToIsoCurveSimpleSWU_G2(u: FP2_BLS381): ECP2_BLS381 =
   ## Implementation of map_to_curve_simple_swu
-  ## for the G2 curve of BLS12-381 curve.
+  ## to map an element of FP2 to a curve isogenous
+  ## to the G2 curve of BLS12-381 curve.
   ##
   ## SWU stands for Shallue-van de Woestijne-Ulas mapping
   ## described in https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-04#section-6.5.2
@@ -155,7 +166,7 @@ func mapToCurveSimpleSWU_G2(u: FP2_BLS381): ECP2_BLS381 =
   ## - u, an element of FP2
   ##
   ## Output:
-  ## - (x, y), a point on G2
+  ## - (x, y), a point on G'2, a curve isogenous to G2 curve of BLS12-381
 
   {.noSideEffect.}: # Only globals accessed are A, B, Z, c1, c2.
                     # we use globals to ensure they are computed only once.
@@ -198,6 +209,70 @@ func mapToCurveSimpleSWU_G2(u: FP2_BLS381): ECP2_BLS381 =
 
   let onCurve = bool ECP2_BLS381_set(addr result, unsafeAddr x, unsafeAddr y)
   assert onCurve
+
+proc isogeny_map_G2(isoPoint: ECP2_BLS381): ECP2_BLS381 =
+  ## 3-isogeny map from a point P' (x', y') on G'2
+  ## to a point P(x, y) on G2 curve of BLS12-381.
+  ##
+  ## https://tools.ietf.org/html/draft-irtf-cfrg-hash-to-curve-05#appendix-C.3
+
+  {.noSideEffect.}: # Globals to ensure they are computed only once
+    # Constants to compute x_numerator
+    let k10 {.global.} = hexToFP2(
+      "0x5c759507e8e333ebb5b7a9a47d7ed8532c52d39fd3a042a88b58423c50ae15d5c2638e343d9c71c6238aaaaaaaa97d6",
+      "0x5c759507e8e333ebb5b7a9a47d7ed8532c52d39fd3a042a88b58423c50ae15d5c2638e343d9c71c6238aaaaaaaa97d6"
+    )
+    let k11 {.global.} = hexToFP2(
+      "0x0",
+      "0x11560bf17baa99bc32126fced787c88f984f87adf7ae0c7f9a208c6b4f20a4181472aaa9cb8d555526a9ffffffffc71a"
+    )
+    let k12 {.global.} = hexToFP2( # The last nibble "e" is not a typo
+      "0x11560bf17baa99bc32126fced787c88f984f87adf7ae0c7f9a208c6b4f20a4181472aaa9cb8d555526a9ffffffffc71e",
+      "0x8ab05f8bdd54cde190937e76bc3e447cc27c3d6fbd7063fcd104635a790520c0a395554e5c6aaaa9354ffffffffe38d"
+    )
+    let k13 {.global.} = hexToFP2(
+      "0x171d6541fa38ccfaed6dea691f5fb614cb14b4e7f4e810aa22d6108f142b85757098e38d0f671c7188e2aaaaaaaa5ed1",
+      "0x0"
+    )
+    # Constants to compute x_denominator
+    let k20 {.global.} = hexToFP2(
+      "0x0",
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaa63"
+    )
+    let k21 {.global.} = hexToFP2( # the last byte "9f" is not a typo
+      "0xc",
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaa9f"
+    )
+    # Constants to compute y_numerator
+    let k30 {.global.} = hexToFP2(
+      "0x1530477c7ab4113b59a4c18b076d11930f7da5d4a07f649bf54439d87d27e500fc8c25ebf8c92f6812cfc71c71c6d706",
+      "0x1530477c7ab4113b59a4c18b076d11930f7da5d4a07f649bf54439d87d27e500fc8c25ebf8c92f6812cfc71c71c6d706"
+    )
+    let k31 {.global.} = hexToFP2(
+      "0x0",
+      "0x5c759507e8e333ebb5b7a9a47d7ed8532c52d39fd3a042a88b58423c50ae15d5c2638e343d9c71c6238aaaaaaaa97be"
+    )
+    let k32 {.global.} = hexToFP2(
+      "0x11560bf17baa99bc32126fced787c88f984f87adf7ae0c7f9a208c6b4f20a4181472aaa9cb8d555526a9ffffffffc71c",
+      "0x8ab05f8bdd54cde190937e76bc3e447cc27c3d6fbd7063fcd104635a790520c0a395554e5c6aaaa9354ffffffffe38f"
+    )
+    let k33 {.global.} = hexToFP2(
+      "0x124c9ad43b6cf79bfbf7043de3811ad0761b0f37a1e26286b0e977c69aa274524e79097a56dc4bd9e1b371c71c718b10",
+      "0x0"
+    )
+    # Constants to compute y_denominator
+    let k40 = hexToFP2(
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffa8fb",
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffa8fb"
+    )
+    let k41 = hexToFP2(
+      "0x0",
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffa9d3"
+    )
+    let k42 = hexToFP2(
+      "0x12",
+      "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaa99"
+    )
 
 # Unofficial test vectors for hashToG2 primitives
 # ----------------------------------------------------------------------
