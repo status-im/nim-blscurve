a8cefc16cd85beb327445cb0923b6008aeac24db
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-01-22 12:42:23 +0100

    Add test vector

--- blscurve/hash_to_curve.nim
+++ blscurve/hash_to_curve.nim
@@ -388,6 +388,8 @@ when isMainModule:
   proc hexToBytes(s: string): seq[byte] =
     if s.len != 0: return hexToSeqByte(s)
 
+  # Test vectors for hashToBaseFP2
+  # ----------------------------------------------------------------------
   template testHashToBaseFP2(id, constants: untyped) =
     # https://github.com/mratsim/py_ecc/pull/1
     proc `test _ id`() =
@@ -410,17 +412,67 @@ when isMainModule:
 
     `test _ id`()
 
-  block: # hashToBaseFP2
-    testHashToBaseFP2 1:
-      let
-        msg = "msg"
-        ctr = 0'i8
-        dst = "BLS_SIG_BLS12381G2-SHA256-SSWU-RO_POP_"
+  # block: # hashToBaseFP2
+  #   testHashToBaseFP2 1:
+  #     let
+  #       msg = "msg"
+  #       ctr = 0'i8
+  #       dst = "BLS_SIG_BLS12381G2-SHA256-SSWU-RO_POP_"
 
-      # Expected output after hkdfExpand
-      let t = [
-        "0x3852c6c62ecd4e04360c24e8ddeac03661b07575a60d6fb7b0a90ce0bb7c7667624fbeea77777e52099dd43356e03192b3d4d27264fd09d0afadda24f48b6f2c",
-        "0x099695b4dc8d5dbebc73a9856cc859a3e5317e9a9e0459ee8fc03646bdcfe30125aa434dda228311f25d8c227d5eee289dd6a50897c08397565bc826c5c4113d"
+  #     # Expected output after hkdfExpand
+  #     let t = [
+  #       "0x3852c6c62ecd4e04360c24e8ddeac03661b07575a60d6fb7b0a90ce0bb7c7667624fbeea77777e52099dd43356e03192b3d4d27264fd09d0afadda24f48b6f2c",
+  #       "0x099695b4dc8d5dbebc73a9856cc859a3e5317e9a9e0459ee8fc03646bdcfe30125aa434dda228311f25d8c227d5eee289dd6a50897c08397565bc826c5c4113d"
+  #     ]
+
+  #     # TODO: doAssert the FP2
+
+  # Test vectors for mapToCurveG2
+  # ----------------------------------------------------------------------
+  template testMapToCurveG2(id, constants: untyped) =
+    # https://github.com/sigp/incubator-milagro-crypto-rust/blob/49563467/src/bls381.rs#L209-L328
+    # Themselves extracted from
+    # https://github.com/kwantam/bls_sigs_ref/tree/master/python-impl
+    proc `test _ id`() =
+      # We create a proc to avoid allocating too much globals.
+      constants
+
+      let u0 = hexToFP2(u0x, u0y)
+      let u1 = hexToFP2(u1x, u1y)
+
+      echo "-----------------------------------"
+      echo "u0: ", u0
+      FP2_BLS381_output(u0.unsafeAddr)
+      echo "\nu1: ", u1
+      FP2_BLS381_output(u1.unsafeAddr)
+      echo ""
+
+
+      let q0 = mapToCurveG2(u0)
+      let q1 = mapToCurveG2(u1)
+
+      echo "-----------------------------------"
+      echo "q0: ", q0
+      echo "q1: ", q1
+
+    `test _ id`()
+
+  block: # hashToBaseFP2
+    testMapToCurveG2 1:
+      let
+        u0x = "0x004ad233c619209060e40059b81e4c1f92796b05aa1bc6358d65e53dc0d657dfbc713d4030b0b6d9234a6634fd1944e7"
+        u0y = "0x0e2386c82713441bc3b06a460bd81850f4bf376ea89c80b18c0881e855c58dc8e83b2fd23af983f4786508e30c42af01"
+        u1x = "0x08a6a75e0a8d32f1e096f29047ea879dd34a5504218d7ce92c32c244786822fb73fbf708d167ad86537468249ec6df48"
+        u1y = "0x07016d0e5e13cd65780042c6f7b4c74ae1c58da438c99582696818b5c229895b893318dcb87d2a65e557d4ebeb408b70"
+
+      # Expected ECP2 (x, y: FP2) affine coordinates
+      let ecp = [
+        # x
+        "0x04861c41efcc5fc56e62273692b48da25d950d2a0aaffb34eff80e8dbdc2d41ca38555ceb8554368436aea47d16056b5",
+        "0x099695b4dc8d5dbebc73a9856cc859a3e5317e9a9e0459ee8fc03646bdcfe30125aa434dda228311f25d8c227d5eee289dd6a50897c08397565bc826c5c4113d",
+        # y
+        "177d05b95e7879a7ddbd83c15114b5a4e9846fde72b2263072dc9e60db548ccbadaacb92cc4952d4f47425fe3c5e0172",
+        "0fc82c99b928ed9df12a74f9215c3df8ae1e9a3fa54c00897889296890b23a0edcbb9653f9170bf715f882b35c0b4647"
       ]
 
       # TODO: doAssert the FP2
@@ -445,18 +497,18 @@ when isMainModule:
 
     `test _ id`()
 
-  block: # hashToBaseFP2
-    testHashToG2 1:
-      let
-        msg = "msg"
-        dst = "BLS_SIG_BLS12381G2-SHA256-SSWU-RO_POP_"
+  # block: # hashToBaseFP2
+  #   testHashToG2 1:
+  #     let
+  #       msg = "msg"
+  #       dst = "BLS_SIG_BLS12381G2-SHA256-SSWU-RO_POP_"
 
-      # Expected output
-      let point = (
-        # x
-        ["0x7896efdac56b0f6cbd8c78841676d63fc733b692628687bf25273aa8a107bd8cb53bbdb705b551e239dffe019abd4df",
-         "0xbd557eda8d16ab2cb2e71cca4d7b343985064daad04734e07da5cdda26610b59cdc0810a25276467d24b315bf7860e0"],
-        # y
-        ["0x1bdb6290cae9f30f263dd40f014b9f4406c3fbbc5fea47e2ebd45e42332553961eb53a15c09e5e090d7a7122dc6657",
-         "18370459c44e799af8ef31634a683e340e79c3a06f912594d287a443620933b47a2a3e5ce4470539eae50f6d49b8ebd6"]
-      )
+  #     # Expected output
+  #     let point = (
+  #       # x
+  #       ["0x7896efdac56b0f6cbd8c78841676d63fc733b692628687bf25273aa8a107bd8cb53bbdb705b551e239dffe019abd4df",
+  #        "0xbd557eda8d16ab2cb2e71cca4d7b343985064daad04734e07da5cdda26610b59cdc0810a25276467d24b315bf7860e0"],
+  #       # y
+  #       ["0x1bdb6290cae9f30f263dd40f014b9f4406c3fbbc5fea47e2ebd45e42332553961eb53a15c09e5e090d7a7122dc6657",
+  #        "18370459c44e799af8ef31634a683e340e79c3a06f912594d287a443620933b47a2a3e5ce4470539eae50f6d49b8ebd6"]
+  #     )
