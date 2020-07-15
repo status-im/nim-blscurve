ee405230b61d002546a7847037bf46f9ee18486d
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-07-15 23:33:26 +0200

    Drop hash-to-curve v5 support

--- tests/test_locator.nim
+++ tests/test_locator.nim
@@ -9,24 +9,14 @@ import
 export
   os, json
 
-when BLS_ETH2_SPEC == "v0.11.x":
-  const ETH2_DIR = currentSourcePath.rsplit(DirSep, 1)[0] / "eth2.0_v0.10.1_vectors"
-else:
-  const ETH2_DIR = currentSourcePath.rsplit(DirSep, 1)[0] / "eth2.0_v0.12.0_vectors"
+const ETH2_DIR = currentSourcePath.rsplit(DirSep, 1)[0] / "eth2.0_v0.12.0_vectors"
 
 proc parseTest*(file: string): JsonNode =
   var yamlStream = openFileStream(file)
   defer: yamlStream.close()
   result = yamlStream.loadToJson()[0]
 
-when BLS_ETH2_SPEC == "v0.12.x":
-  const SkippedTests = [""]
-else: # Buggy tests upstream
-  const SkippedTests = [
-    "small"/"fast_aggregate_verify_e6922a0d196d9869"/"data.yaml", # Buggy upstream vector: https://github.com/ethereum/eth2.0-specs/issues/1618
-    "small"/"fast_aggregate_verify_62bca7cd61880e26"/"data.yaml",
-    "small"/"fast_aggregate_verify_3b2b0141e95125f0"/"data.yaml",
-  ]
+const SkippedTests = [""]
 
 iterator walkTests*(category: string, skipped: var int): (string, string) =
   let testDir = ETH2_DIR / category
