65054fb3588bbf1fe1743e68f512d85f85ece449
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-09-17 12:30:14 +0200

    Remove aliasing workaround (to be confirmed in NBC CI)

--- blscurve/blst/blst_lowlevel.nim
+++ blscurve/blst/blst_lowlevel.nim
@@ -9,14 +9,6 @@
 
 import std/os
 
-when defined(gcc):
-  # * Using ftree-loop-vectorize will miscompile scalar multiplication
-  #   for example used to derive the public key in blst_sk_to_pk_in_g1
-  # * Using ftree-slp-vectorize miscompiles something when used
-  #   in nim-beacon-chain in Travis CI (TODO: test case)
-  # no-tree-vectorize removes both
-  {.passC: "-fno-tree-vectorize".}
-
 {.compile: ".."/".."/"vendor"/"blst"/"build"/"assembly.S".}
 {.compile: ".."/".."/"vendor"/"blst"/"src"/"server.c".}
 
