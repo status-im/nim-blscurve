378e6e042d48279a399ef8eb8ca422adc569323b
mratsim <mamy_github@numforge.co>
2020-10-26 18:04:45 +0100

    Upgade Milagro vectors

--- tests/eth2_vectors.nim
+++ tests/eth2_vectors.nim
@@ -44,59 +44,74 @@ template testGen*(name, testJson, body: untyped): untyped =
     if skipped > 0:
       echo "[Warning]: ", skipped, " tests skipped."
 
-proc getFrom(T: typedesc, test: JsonNode, inout: static InOut): T =
+proc getFrom(T: typedesc, test: JsonNode, inout: static InOut): tuple[val: T, ok: bool] =
   when inout == Output:
     when T is bool:
-      result = test["output"].getBool()
+      result = (test["output"].getBool(), true)
     else:
-      let maybeParsed = result.fromHex(test["output"].getStr())
-      if not maybeParsed: # We might read an empty string for N/A pubkeys
-        doAssert test["output"].getStr() == "",
-          "Couldn't parse output " & $T & ": " & test["output"].getStr()
+      result.ok = result.val.fromHex(test["output"].getStr())
+      # if not result.ok: # We might read an empty string for N/A pubkeys
+      #   echo "Couldn't parse output " & $T & ": " & test["output"].getStr()
   else:
     when T is seq[Signature]:
       for sigHex in test["input"]:
-        result.setLen(result.len + 1)
-        doAssert result[^1].fromHex(sigHex.getStr()),
+        result.val.setLen(result.val.len + 1)
+        doAssert result.val[^1].fromHex(sigHex.getStr()),
           "Couldn't parse input Signature: " & sigHex.getStr()
+      result.ok = true
     else:
       {.error: "Unreachable".}
 
-proc getFrom(T: typedesc, test: JsonNode, inout: static InOut, name: string): T =
+proc getFrom(T: typedesc, test: JsonNode, inout: static InOut, name: string): tuple[val: T, ok: bool] =
   when inout == Output:
     {.error: "Unreachable".}
   else:
     when T is seq[byte]:
-      result = test["input"][name].getStr().hexToSeqByte()
+      result = (test["input"][name].getStr().hexToSeqByte(), true)
     elif T is seq[PublicKey]:
       for pubKeyHex in test["input"][name]:
-        result.setLen(result.len + 1)
-        doAssert result[^1].fromHex(pubKeyHex.getStr()),
-          "Couldn't parse input PublicKey: " & pubKeyHex.getStr()
+        result.val.setLen(result.val.len + 1)
+        let ok = result.val[^1].fromHex(pubKeyHex.getStr())
+        if not ok:
+          # echo "Couldn't parse input PublicKey: " & pubKeyHex.getStr()
+          result.ok = false
+          return
+      result.ok = true
     else:
-      doAssert result.fromHex(test["input"][name].getStr()),
-          "Couldn't parse input '" & name & "' (" & $T &
-          "): " & test["input"][name].getStr()
+      result.ok = result.val.fromHex(test["input"][name].getStr())
+      # if not result.ok:
+      #   echo "Couldn't parse input '" & name & "' (" & $T &
+      #     "): " & test["input"][name].getStr()
 
-proc aggFrom(T: typedesc, test: JsonNode): T =
+proc aggFrom(T: typedesc, test: JsonNode): tuple[val: T, ok: bool] =
   when T is seq[(PublicKey, seq[byte])]:
     for pubkey in test["input"]["pubkeys"]:
-      result.setLen(result.len + 1)
-      doAssert result[^1][0].fromHex(pubkey.getStr()),
-          "Couldn't parse input PublicKey: " & pubkey.getStr()
+      result.val.setLen(result.val.len + 1)
+      let ok = result.val[^1][0].fromHex(pubkey.getStr())
+      if not ok:
+        # echo "Couldn't parse input PublicKey: " & pubkey.getStr()
+        result.ok = false
+        return
+
     var i = 0
     for message in test["input"]["messages"]:
-      result[i][1] = message.getStr().hexToSeqByte()
+      result.val[i][1] = message.getStr().hexToSeqByte()
       inc i
+    result.ok = true
   elif T is seq[PublicKey]:
     for pubkey in test["input"]["pubkeys"]:
-      result.setLen(result.len + 1)
-      doAssert result[^1].fromHex(pubkey.getStr()),
-          "Couldn't parse input PublicKey: " & pubkey.getStr()
+      result.val.setLen(result.val.len + 1)
+      let ok = result.val[^1].fromHex(pubkey.getStr())
+      if not ok:
+        # echo "Couldn't parse input PublicKey: " & pubkey.getStr()
+        result.ok = false
+        return
+    result.ok = true
   elif T is seq[seq[byte]]:
     for message in test["input"]["messages"]:
-      result.setLen(result.len + 1)
-      result[^1] = message.getStr().hexToSeqByte()
+      result.val.setLen(result.val.len + 1)
+      result.val[^1] = message.getStr().hexToSeqByte()
+    result.ok = true
   else:
     {.error: "Unreachable".}
 
@@ -107,110 +122,98 @@ testGen(sign, test):
 
     expectedSig = Signature.getFrom(test, Output)
 
-  let libSig = privKey.sign(message)
+  doAssert privkey.ok == expectedSig.ok
+  if not privkey.ok or not expectedSig.ok:
+    return
 
-  doAssert libSig == expectedSig, block:
+  let libSig = privKey.val.sign(message.val)
+
+  doAssert libSig == expectedSig.val, block:
     "\nSignature differs from expected \n" &
     "   computed: " & libSig.toHex() & "\n" &
-    "   expected: " & expectedSig.toHex()
+    "   expected: " & expectedSig.val.toHex()
 
 testGen(verify, test):
-  let expected = bool.getFrom(test, Output)
-  var
-    pubkey: PublicKey
-    message: seq[byte]
-    signature: Signature
-  try:
+  let
+    expected = bool.getFrom(test, Output)
     pubKey = PublicKey.getFrom(test, Input, "pubkey")
     message = seq[byte].getFrom(test, Input, "message")
     signature = Signature.getFrom(test, Input, "signature")
-  except:
-    let emsg = getCurrentExceptionMsg()
-    if expected:
-      doAssert false, "Verification was not supposed to fail, but one of the inputs was invalid." & emsg
-    else:
-      echo "[INFO] Expected verification failure at parsing stage: " & emsg
 
-  let libValid = pubKey.verify(message, signature)
+  if not pubkey.ok:
+    # Infinity pubkey and infinity signature
+    doAssert not expected.val
+    return
 
-  doAssert libValid == expected, block:
+  let libValid = pubKey.val.verify(message.val, signature.val)
+
+  doAssert libValid == expected.val, block:
     "\nVerification differs from expected \n" &
     "   computed: " & $libValid & "\n" &
-    "   expected: " & $expected
+    "   expected: " & $expected.val
 
 testGen(aggregate, test):
   let sigs = seq[Signature].getFrom(test, Input)
   let expectedAgg = Signature.getFrom(test, Output)
 
-  # TODO - at which level should we catch the empty signatures?
-  if sigs.len == 0:
-    echo "       ⚠⚠⚠ Skipping empty aggregation, handled at the client level"
+  var libAggSig {.noInit.}: Signature
+  let ok = libAggSig.aggregateAll(sigs.val)
+  if not ok:
+    doAssert not expectedAgg.ok
+    doAssert sigs.val.len == 0
     return
 
-  let libAggSig = aggregate(sigs)
-
-  doAssert libAggSig == expectedAgg, block:
+  doAssert libAggSig == expectedAgg.val, block:
     "\nSignature aggregation differs from expected \n" &
     "   computed: " & libAggSig.toHex() & "\n" &
-    "   expected: " & expectedAgg.toHex()
+    "   expected: " & expectedAgg.val.toHex()
 
 testGen(fast_aggregate_verify, test):
-  let expected = bool.getFrom(test, Output)
-  var
-    pubkeys: seq[PublicKey]
-    message: seq[byte]
-    signature: Signature
-  try:
+  let
+    expected = bool.getFrom(test, Output)
     pubKeys = seq[PublicKey].getFrom(test, Input, "pubkeys")
     message = seq[byte].getFrom(test, Input, "message")
     signature = Signature.getFrom(test, Input, "signature")
-  except:
-    let emsg = getCurrentExceptionMsg()
-    if expected:
-      doAssert false, "Verification was not supposed to fail, but one of the inputs was invalid." & emsg
-    else:
-      echo "[INFO] Expected verification failure at parsing stage: " & emsg
 
-  let libValid = pubKeys.fastAggregateVerify(message, signature)
+  if not pubkeys.ok:
+    # Infinity pubkey in the mix
+    doAssert not expected.val
+    return
 
-  doAssert libValid == expected, block:
+  let libValid = pubKeys.val.fastAggregateVerify(message.val, signature.val)
+
+  doAssert libValid == expected.val, block:
     "\nFast Aggregate Verification differs from expected \n" &
     "   computed: " & $libValid & "\n" &
-    "   expected: " & $expected
+    "   expected: " & $expected.val
 
 testGen(aggregate_verify, test):
-  let expected = bool.getFrom(test, Output)
-  var
+  let
+    expected = bool.getFrom(test, Output)
     # We test both the SoA and AoS API
-    pubkey_msg_pairs: seq[(PublicKey, seq[byte])]
-    pubkeys: seq[PublicKey]
-    msgs: seq[seq[byte]]
-    signature: Signature
-
-  try:
     pubkey_msg_pairs = seq[(PublicKey, seq[byte])].aggFrom(test)
     pubkeys = seq[PublicKey].aggFrom(test)
     msgs = seq[seq[byte]].aggFrom(test)
     signature = Signature.getFrom(test, Input, "signature")
-  except:
-    let emsg = getCurrentExceptionMsg()
-    if expected:
-      doAssert false, "Verification was not supposed to fail, but one of the inputs was invalid." & emsg
-    else:
-      echo "[INFO] Expected verification failure at parsing stage: " & emsg
 
-  let libAoSValid = aggregateVerify(pubkey_msg_pairs, signature)
-  let libSoAValid = aggregateVerify(pubkeys, msgs, signature)
+  let libAoSValid = aggregateVerify(pubkey_msg_pairs.val, signature.val)
+  let libSoAValid = aggregateVerify(pubkeys.val, msgs.val, signature.val)
 
-  doAssert libAoSValid == expected, block:
+  if not pubkeys.ok:
+    # Infinity pubkey in the mix
+    doAssert not pubkey_msg_pairs.ok
+    doAssert not expected.val
+    return
+
+  doAssert libAoSValid == expected.val, block:
     "\nAggregate Verification differs from expected \n" &
     "   computed: " & $libAoSValid & "\n" &
-    "   expected: " & $expected
+    "   expected: " & $expected.val
 
-  doAssert libSoAValid == expected, block:
+  doAssert libSoAValid == expected.val, block:
     "\nAggregate Verification differs from expected \n" &
     "   computed: " & $libSoAValid & "\n" &
-    "   expected: " & $expected
+    "   expected: " & $expected.val
 
 suite "ETH 2.0 " & BLS_ETH2_SPEC & " test vectors - " & $BLS_BACKEND:
   test "[" & BLS_ETH2_SPEC & "] sign(SecretKey, message) -> Signature":
