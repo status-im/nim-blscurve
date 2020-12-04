82b46aa5f2f008052ddffa5a3ae942fcc0cccb02
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-12-04 17:10:47 +0100

    Revert "Use the new BLST no assembly fallback on 32-bit"
    This reverts commit c8b061958e5d770f65392459510fda255fcde74b.

--- blscurve/bls_backend.nim
+++ blscurve/bls_backend.nim
@@ -9,23 +9,28 @@
 
 import os
 
-const BLS_FORCE_BACKEND*{.strdefine.} = "blst"
+const BLS_FORCE_BACKEND*{.strdefine.} = "auto"
 
-static: doAssert BLS_FORCE_BACKEND == "miracl" or
+static: doAssert BLS_FORCE_BACKEND == "auto" or
+                 BLS_FORCE_BACKEND == "miracl" or
                  BLS_FORCE_BACKEND == "blst",
-                 """Only blst" and "miracl" backends are valid."""
+                 """Only "auto", "blst" and "miracl" backends are valid."""
 
 type BlsBackendKind* = enum
   BLST
   Miracl
 
-when BLS_FORCE_BACKEND == "blst" and (
-      gorgeEx(getEnv("CC", "gcc") & " -march=native -dM -E -x c /dev/null | grep -q SSSE3").exitCode == 0
+when BLS_FORCE_BACKEND == "blst" or (
+  BLS_FORCE_BACKEND == "auto" and
+    sizeof(int) == 8 and
+    (defined(arm64) or (
+      defined(amd64) and
+      gorgeEx(getEnv("CC", "gcc") & " -march=native -dM -E -x c /dev/null | grep -q SSSE3").exitCode == 0))
   ):
   # BLST supports: x86_64 and ARM64
   # and has optimized SHA256 routines for x86_64 CPU with SSE3
   const BLS_BACKEND* = BLST
-elif BLS_FORCE_BACKEND == "blst":
+elif BLS_FORCE_BACKEND == "auto" and defined(amd64):
   # CPU doesn't support SSE3 which is used in optimized SHA256
   const BLS_BACKEND* = BLST
   {.passC: "-D__BLST_PORTABLE__".}
