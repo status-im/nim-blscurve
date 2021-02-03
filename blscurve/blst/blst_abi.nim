7e8de1e748e75a977ff4ca9aed798fc6f976fc1c
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2021-02-03 09:57:10 +0100

    enable cross-compilation (#105)

--- blscurve/blst/blst_abi.nim
+++ blscurve/blst/blst_abi.nim
@@ -2,7 +2,7 @@
 # Manual edits
 import std/[strutils, os]
 
-const headerPath = currentSourcePath.rsplit(DirSep, 1)[0]/".."/".."/"vendor"/"blst"/"bindings"/"blst.h"
+const headerPath = currentSourcePath.rsplit({DirSep, AltSep}, 1)[0] & "/../../vendor/blst/bindings/blst.h"
 
 {.pragma: blst, importc, header: headerPath.}
 
