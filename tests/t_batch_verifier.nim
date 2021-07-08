6ab91fb8e2e9dd59b25f3b73b7557edb6ec8bfe7
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2021-07-08 17:42:27 +0200

    Add test for batch signature forgery

--- tests/t_batch_verifier.nim
+++ tests/t_batch_verifier.nim
@@ -9,11 +9,13 @@
 
 import
   # Standard library
-  std/unittest,
+  std/[unittest, random],
   # Status libraries
   taskpools,
   # Public API
-  ../blscurve
+  ../blscurve,
+  # Internal
+  ../blscurve/blst/blst_abi
 
 # Tests for batch verification
 
@@ -130,6 +132,100 @@ suite "Batch verification":
 
     batch.add((pubkey1, msg1, sig1))
     batch.add((pubkey2, msg2, sig1)) # <--- wrong signature
+    check:
+      not tp.batchVerify(cache, batch, fakeRandomBytes)
+      not tp.batchVerify(batch, fakeRandomBytes)
+
+  tp.shutdown()
+
+# ---------------------------------------------------------
+#
+# Malicious forged signatures:
+# https://ethresear.ch/t/fast-verification-of-multiple-bls-signatures/5407
+#
+# We call M, PK, S and AS:
+# messages, public keys, signatures and aggregated signatures.
+#
+# We have:
+# - PK1 verifying the pair (S1, M1)
+# - PK2 verifying the pair (S2, M2)
+# - PK1+PK2 verifying the aggregated (S1+S2, M1+M2)
+#
+# Due to pairing bilinearity, this also means that forged signatures
+# S1 + S' and S2 - S' would verify when aggregated
+# i.e. PK1+PK2 verifying the aggregated ((S1+S')+(S2-S'), M1+M2)
+
+proc genForgedPair(batch: var seq[SignatureSet],
+                   seed1: int, message1: string,
+                   seed2: int, message2: string) =
+  # Generate (PK1, S1+S', M1)
+  # and      (PK2, S2-S', M2)
+  # that would pass naive PK1+PK2 aggregate verification.
+
+  let (pk1, sk1) = keyGen(seed1.uint64)
+  let hashed1 = hash(message1)
+  let sig1 = sk1.sign(hashed1)
+
+  let (pk2, sk2) = keyGen(seed2.uint64)
+  let hashed2 = hash(message2)
+  let sig2 = sk2.sign(hashed2)
+
+  # Forged signature S'
+  let (pkp, skp) = keyGen(uint64(seed1*seed2 + seed1 + seed2))
+  let sigp = skp.sign(hash("rekt"))
+
+  # Check that the forged signature can be verified when naively aggregated
+  # 1. Create -S'
+  var neg_sigp = sigp
+  let neg_sigp_ptr = cast[ptr blst_p2_affine](neg_sigp.addr)
+  neg_sigp_ptr.y.blst_fp2_cneg(neg_sigp_ptr.y, 1)
+
+  # 2. Forge signatures S1+S' and S2-S'
+  var forgedSig1s, forgedSig2ns: Signature
+  doAssert forgedSig1s.aggregateAll([sig1, sigp])
+  doAssert forgedSig2ns.aggregateAll([sig2, cast[Signature](neg_sigp)])
+
+  # 3. Aggregate forged signatures
+  var aggForged: Signature
+  doAssert aggForged.aggregateAll([forgedSig1s, forgedSig2ns])
+
+  # 4. Naive aggregation
+  doAssert aggregateVerify(
+    [pk1, pk2],
+    [@hashed1, @hashed2],
+    aggForged
+  ), "The forged aggregate signature should pass naive verification."
+
+  # Now add to a proper batch verification set
+  batch.add((pk1, hashed1, forgedSig1s))
+  batch.add((pk2, hashed2, forgedSig2ns))
+
+suite "Batch forged signatures":
+  var tp = Taskpool.new(numThreads = 4)
+
+  wrappedTest "Single forged pair":
+    var cache: BatchedBLSVerifierCache
+    var batch: seq[SignatureSet]
+
+    batch.genForgedPair(1, "msg1", 2, "msg2")
+
+    check:
+      not tp.batchVerify(cache, batch, fakeRandomBytes)
+      not tp.batchVerify(batch, fakeRandomBytes)
+
+  wrappedTest "One forgery among many signatures":
+    var cache: BatchedBLSVerifierCache
+    var batch: seq[SignatureSet]
+
+    var rng = initRand(1234)
+
+    for i in 0 ..< 16:
+      batch.addExample(i, "msg" & $i)
+    batch.genForgedPair(1, "msg100", 2, "msg200")
+
+    # Randomize the order
+    rng.shuffle(batch)
+
     check:
       not tp.batchVerify(cache, batch, fakeRandomBytes)
       not tp.batchVerify(batch, fakeRandomBytes)
