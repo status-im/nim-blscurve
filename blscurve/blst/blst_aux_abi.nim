9ffb9a61894792192a148c7a217d51183d0ed09c
Etan Kissling <etan@nimbus.team>
2026-07-29 00:30:28 +0200

    Bump blst to v0.3.17 (#191)
    - https://github.com/supranational/blst/releases/tag/v0.3.17

--- blscurve/blst/blst_aux_abi.nim
+++ blscurve/blst/blst_aux_abi.nim
@@ -1,8 +1,6 @@
 # blst_aux.h lists unstable interfaces that might be promoted to blst.h depending on their worthiness
 # This assumes blst_abi is included
 
-const auxHeaderPath = currentSourcePath.rsplit({DirSep, AltSep}, 1)[0] & "/../../vendor/blst/bindings/blst_aux.h"
-
 {.push cdecl, importc, header: headerPath.}
 
 proc blst_derive_master_eip2333*[T: byte|char](
